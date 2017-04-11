6f717e668f105330d275b15d61f29d3675b26efb
David Rigert <rigertd@onid.orst.edu>
2017-04-10 20:50:03 -0700

    Replaced alias with variable in script.

--- project1/benchmark.sh
+++ project1/benchmark.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# Set alias for GCC 5.4
+GPP5="/usr/local/common/gcc-5.4.0/bin/g++"
 # Remove any existing results, hiding any error message
 rm -f project1.csv 2>/dev/null
 # Number of threads
@@ -9,7 +11,7 @@ do
 	for s in 2 4 8 16 32 64 128 256 512 1024 2048 3072 4096 6144 8192 10240
 	do
 		echo NUMS=$s
-		g++5 -DNUMNODES=$s project1.cpp -o prog -O3 -lm -fopenmp
+		$GPP5 -DNUMNODES=$s project1.cpp -o prog -O3 -lm -fopenmp
 		./prog $t >> project1.csv
 	done
 done
