a3e2f9962b38af73c25bacf8f273f651fa53fdf6
David Rigert <rigertd@onid.orst.edu>
2017-04-15 20:44:56 -0700

    Updated benchmark script.

--- project1/benchmark.sh
+++ project1/benchmark.sh
@@ -3,15 +3,15 @@
 GPP5="/usr/local/common/gcc-5.4.0/bin/g++"
 # Remove any existing results, hiding any error message
 rm -f project1.csv 2>/dev/null
-# Number of threads
-for t in 1 2 4 6 8
+# Number of nodes
+for s in 2 4 8 16 32 64 128 256 512 1024 2048 3072 4096 6144 8192 10240
 do
-	echo NUMT=$t
-	# Number of nodes
-	for s in 2 4 8 16 32 64 128 256 512 1024 2048 3072 4096 6144 8192 10240
-	do
-		echo NUMS=$s
-		$GPP5 -DNUMNODES=$s project1.cpp -o prog -O3 -lm -fopenmp
-		./prog $t >> project1.csv
-	done
+	echo NUMS=$s
+	$GPP5 -DNUMNODES=$s project1.cpp -o prog -O0 -lm -fopenmp
+    # Number of threads
+    for t in 1 2 4 6 8
+    do
+        echo NUMT=$t
+    	./prog $t >> project1.csv
+    done
 done
