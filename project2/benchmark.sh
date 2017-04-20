f6c45b2242b7e01e3ef2854d983ed87c4113d97c
David Rigert <rigertd@oregonstate.edu>
2017-04-19 20:54:31 -0700

    Converted tabs to spaces.

--- project2/benchmark.sh
+++ project2/benchmark.sh
@@ -6,15 +6,15 @@ for s in static dynamic
 do
     echo SCHEDULING=$s
     # chunksize
-	for c in 1 4096
-	do
-		echo CHUNKSIZE=$c
-		g++ -DSCHEDULING=$s -DCHUNKSIZE=$c project2.cpp -o prog -O1 -lm -fopenmp
-		# Number of threads
-		for t in 1 2 4 6 8 10 12 16
-		do
-			echo NUMT=$t
-			./prog $t >> project2.csv
-		done
+    for c in 1 4096
+    do
+        echo CHUNKSIZE=$c
+        g++ -DSCHEDULING=$s -DCHUNKSIZE=$c project2.cpp -o prog -O1 -lm -fopenmp
+        # Number of threads
+        for t in 1 2 4 6 8 10 12 16
+        do
+            echo NUMT=$t
+            ./prog $t >> project2.csv
+        done
     done
 done
