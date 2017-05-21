85f76dca9514470e8710d746c304354bae6e0c99
David Rigert <rigertd@oregonstate.edu>
2017-05-21 11:57:55 -0700

    Added additional data point to script

--- project5/benchmark.sh
+++ project5/benchmark.sh
@@ -4,7 +4,7 @@ rm -f project5.csv 2>/dev/null
 # Output header row to CSV
 echo "SSE","Reduction","Array Size","Avg MegaMults/Sec","Peak MegaMults/Sec" >> project5.csv
 # Run for different array sizes, from 1024 to 33554432
-for arr in 1024 2048 4096 8192 16384 32768 65536 131072 262144 524288 1048576 2097152 4194304 6291456 8388608 12582912 16777216 33554432
+for arr in 1024 2048 4096 8192 16384 32768 65536 131072 262144 524288 1048576 2097152 4194304 6291456 8388608 12582912 16777216 25165824 33554432
 do
     echo ARR_SIZE=$arr
     g++ -std=c++11 -DARR_SIZE=$arr project5.cpp simd.p5.cpp mulsum.p5.cpp -o prog -lm -fopenmp
